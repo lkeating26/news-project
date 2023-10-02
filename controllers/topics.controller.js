068a85aab29310cbf687f2675a88171507ae37d1
lkeating26 <114599878+lkeating26@users.noreply.github.com>
2023-10-02 14:48:00 +0100

    2-CORE-GET-/api/topics complete

--- controllers/topics.controller.js
+++ controllers/topics.controller.js
@@ -1,7 +1,6 @@
 const { selectTopics } = require('../models/topics.models');
 
 const getTopics = (req, res, next) => {
-    console.log('in controller')
     selectTopics().then((topics) => {
         res.status(200).send({ topics });
     });
