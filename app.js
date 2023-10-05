160e768fb4f8d4898470c2ec0d471c88d46487ee
lkeating26 <114599878+lkeating26@users.noreply.github.com>
2023-10-05 14:43:01 +0100

    Revert "6-CORE-GET-/api/articles/article_id/comments complete"

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const express = require('express')
 const { getTopics } = require('./controllers/topics.controller')
 const { getApis } = require('./controllers/api.controllers')
 const { getArticles, getArticleById } = require('./controllers/articles.controller')
-const { getComments } = require('./controllers/comments.controllers')
 const { handlePsqlErrors, handleCustomErrors } = require('./errors/index')
 
 const app = express()
@@ -13,8 +12,6 @@ app.get('/api/topics', getTopics);
 app.get('/api/articles', getArticles);
 
 app.get('/api/articles/:article_id', getArticleById);
-
-app.get('/api/articles/:article_id/comments', getComments);
 
 app.get('/api', getApis);
 
